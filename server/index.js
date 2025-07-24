59b22aabf6c2bbda3cdd84cdcce3809e0c2a4400
SHILAT-GIT <shilatavr2002@gmail.com>
2025-07-24 15:47:45 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path');
+const app = express()
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectdb = require('./db');
+
+connectdb();
+
+// הגדרת נתיב לתיקיית הסטטיים
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+//משמש לקריאת טפסים שאינם json
+const bodyParser = require('body-parser')
+
+//מאפשר להשתמש באובייקטים מורכבים
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// הגדרת נתיב לבית
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'homePage.html'));
+});
+
+//  ייבוא הראוטים
+const userRoutes = require('./routes/userRoutes');
+const perfumeRoutes = require('./routes/perfumeRoutes');
+
+
+//  חיבור הראוטים
+app.use('/api/users', userRoutes);
+app.use('/api/perfumes', perfumeRoutes);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server is running")
+})
+
+
---
+++ server/models/Perfume.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const perfumeSchema = new mongoose.Schema({
+    
+});
+
+module.exports = mongoose.model('Perfume', perfumeSchema);
