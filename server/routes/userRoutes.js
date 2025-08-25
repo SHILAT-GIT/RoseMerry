6c5da0c62c939c3b9feb1d4017bd3c2e81856d22
SHILAT-GIT <shilatavr2002@gmail.com>
2025-08-25 19:55:17 +0300

    הוספת פונקציה לשליפת היוזרים
    הוספתי פונקציה שאין לנו שימוש איתה לשליפת היוזרים כדי שבמונגו יתווסף לנו התקייה טסט שבה אפשר להוסיף משתמשים
    
    בהמשך נמחק אותה

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
+router.get('/users', async (_, res) => {
+    try {
+        const users = await Apartment.find();
+        res.send({ users });
+    } catch (err) {
+        res.status(500).send({ message: 'שגיאה בשרת' });
+    }
+});
+
 module.exports = router;
